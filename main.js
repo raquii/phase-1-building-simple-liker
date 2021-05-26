a694af82651ab8224fa023c94e604f1fa448925c
raquel <raquelanaroman@gmail.com>
2021-05-26 13:31:50 -0400

    commit tree 9e6f127073d8d998bfcb9be09afa8777c44a76a0

--- main.js
+++ main.js
@@ -2,7 +2,44 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+//declare variables for elements that need selected.
+const heartGlyphs = document.querySelectorAll('.like-glyph');
+const errorModal = document.querySelector('#modal');
+
+//add a click event listener to the .like-glyphs
+//this has to be inside of a loop to apply the listener to multiple elements in a clean fashion
+//write callback function for click event listener
+heartGlyphs.forEach(function(ele){
+  ele.addEventListener('click', function(e) {
+      //CBF invokes mimicServerCall if heart is empty
+      mimicServerCall("URL")
+      //write then that changes heart to full and adds .activated-heart class
+        .then(function(){
+          if(e.target.innerText === EMPTY_HEART){
+            e.target.innerText = FULL_HEART;
+            e.target.classList.add("activated-heart");
+          }else if(e.target.innerText === FULL_HEART){
+            //if heart is full, CBF removes.activated-heart class and changes back to empty heart
+            e.target.classList.remove("activated-heart")
+            e.target.innerText = EMPTY_HEART;
+          }; 
+        })
+        .catch(function(error){
+          //write catch that removes the .hidden class from error modal and displays the server message
+          errorModal.className = '';
+          errorModal.innerText = error;
+          //write setTimeout to hide modal and message after 3 seconds
+          setTimeout(() => errorModal.className = "hidden", 3000);
+        });
+  });
+});
+
+
+
+
+
+
+
 
 
 
